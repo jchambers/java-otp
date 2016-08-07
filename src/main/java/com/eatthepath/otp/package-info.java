51fe5716a45bf8a8a1150b3f1d38193063230269
Jon Chambers <jon.chambers@gmail.com>
2016-08-07 12:24:56 -0400

    Added (minimal) package-level docs.

--- src/main/java/com/eatthepath/otp/package-info.java
+++ src/main/java/com/eatthepath/otp/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
- */
-/**
- * @author <a href="https://github.com/jchambers">Jon Chambers</a>
+ * Contains classes for generating one-time passwords using either the
+ * <a href="https://tools.ietf.org/html/rfc4226">HOTP (RFC&nbsp;4226)</a> or
+ * <a href="https://tools.ietf.org/html/rfc6238">TOTP (RFC&nbsp;6328)</a> algorithms.
  *
+ * @author <a href="https://github.com/jchambers">Jon Chambers</a>
  */
 package com.eatthepath.otp;
