2b5ddc9011173eb000f5ae05e5c4ed4513be0346
Jon Chambers <jon.chambers@gmail.com>
2022-07-31 12:51:11 -0400

    Add docs for 0.4

---
+++ apidocs/0.4/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.eatthepath.otp","l":"HmacOneTimePasswordGenerator"},{"p":"com.eatthepath.otp","l":"TimeBasedOneTimePasswordGenerator"},{"p":"com.eatthepath.otp","l":"UncheckedNoSuchAlgorithmException"}];updateSearchResults();
